6fc250adbdbb7f2bda9acb59151c36459f89f6c9
Diksi Erlangga <diksi131@gmail.com>
2021-03-25 13:24:34 +0800

    Custom Footer (40. Working with Footers in WordPress)
    Patuh care ngae header

---
+++ footer-landing.php
@@ -0,0 +1,6 @@
+    <div class="footer-container">
+        <div class="footer-content"><p>This is Kaki Mendarat</p></div>
+    </div>
+    
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php get_header() ?>
 
     <h1>index.euy</h1>
-    
-</body>
-</html>
+
+<?php get_footer() ?>
