98d0195b7dd4376cda5a0fc3530e63a6207c1270
Diksi Erlangga <diksi131@gmail.com>
2021-03-25 11:49:06 +0800

    Edit functions.php (37. Working with the functions.php file)
    tambah add_theme_support dan load CSS

--- functions.php
+++ functions.php
@@ -1,3 +1,22 @@
 <?php
+    //Add Theme Support
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'post_format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
+    add_theme_support( 'html5' );
+    add_theme_support( 'automated-feed-links' );
+    add_theme_support( 'custom-background' );
+    add_theme_support( 'custom-header' );
+    add_theme_support( 'custom-logo' );
+    add_theme_support( 'customize-selective-refresh-widgets' );
+    add_theme_support( 'starter_content' );
 
+    //Load CSSS
+    function wphierarchy_enqueue_style() {
+    
+        wp_enqueue_style( 'main-css', get_stylesheet_directory_uri(), '/style.css', [], time(), 'all' );
+
+    }
+
+    add_action( 'wp_enqueue_scripts', 'wphierarchy_enqueue_style' );
 ?>
