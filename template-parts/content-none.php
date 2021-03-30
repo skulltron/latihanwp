365af1c939251490adb4e55f505931e82d2e78b6
Diksi Erlangga <diksi131@gmail.com>
2021-03-30 13:00:37 +0800

    Template Parts (48. Creating Content Includes)
    add new content file and use get_template_part to insert content in index.php (makes code look cleaner)

---
+++ template-parts/content-none.php
@@ -0,0 +1,15 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class();?>>
+            
+                <header class="entry-header">
+
+                    <h1><?php esc_html_e( '404 Not Found', 'wphierarchy' ); ?></h1>
+                        
+                </header>
+
+                <div class="entry-content">
+                
+                    <p><?php esc_html_e( 'Are you lost ?', 'wphierarchy' ); ?></p>
+
+                </div>
+            
+            </article>
