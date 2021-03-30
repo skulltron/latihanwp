4e3429b35d8ecf6bab0f057d24e424bcce87dd8d
Diksi Erlangga <diksi131@gmail.com>
2021-03-30 10:29:55 +0800

    Edit header.php and footer.php (42. Adding Markup to a Theme - Part 1)
    add header and content markups (site title & site description from bloginfo, separate content and header)

--- footer.php
+++ footer.php
@@ -1,8 +1,12 @@
+    </div> <!--#content-->
+    
     <div class="footer-container">
         <div class="footer-content"><p>This is Kaki</p></div>
     </div>
     
     <?php wp_footer(); ?>
+
+    </div> <!--#page-->
     
     </body>
 </html>
--- header.php
+++ header.php
@@ -9,11 +9,30 @@
 </head>
 <body <?php body_class(); ?>>
    
-    <nav id="site-navigation" class="main-navigation" role="navigation">
-        <?php
-            $args = [
-                'theme-location' => 'main-menu'
-            ];
-            wp_nav_menu( $args )
-        ?>
-    </nav>
+    <div id="page">
+
+        <a href="#content" class="skip-link screen-reader-text">
+            <?php esc_html__( 'Skip to Content', 'wphierarchy' )?>
+        </a>
+
+        <header id="masthead" class="site-header" role="banner">
+
+            <div class="site-branding">
+                <a href="<?php echo esc_url( home_url( '/' ) );?>" rel="home">
+                    <p class="site-title"><?php bloginfo( 'name' ); ?></p>
+                </a>
+                <p class="site-description"><?php bloginfo( 'description' )?></p>
+            </div>
+
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <?php
+                    $args = [
+                        'theme-location' => 'main-menu'
+                    ];
+                    wp_nav_menu( $args )
+                ?>
+            </nav>
+
+        </header>
+        
+        <div id="content">
