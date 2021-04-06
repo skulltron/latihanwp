fc5ba9b8188e975dabec958ce076da196cb1d722
Diksi Erlangga <diksi131@gmail.com>
2021-04-06 10:14:04 +0800

    Show Post Formats (53. Working with Post Formats in WordPress)
    added code for showing post formats in content-page.php

--- functions.php
+++ functions.php
@@ -2,14 +2,14 @@
     //Add Theme Support
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
-    add_theme_support( 'post_format', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
+    add_theme_support( 'post-formats', ['aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'] );
     add_theme_support( 'html5' );
     add_theme_support( 'automated-feed-links' );
     add_theme_support( 'custom-background' );
     add_theme_support( 'custom-header' );
     add_theme_support( 'custom-logo' );
     add_theme_support( 'customize-selective-refresh-widgets' );
-    add_theme_support( 'starter_content' );
+    add_theme_support( 'starter-content' );
 
     //Load CSSS
     function wphierarchy_enqueue_style() {
--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -2,6 +2,8 @@
             
                 <header class="entry-header">
 
+                <?php echo get_post_format( $post->ID ); ?>
+
                     <?php the_title( '<h1>', '</h1>'); ?>
                     
                 </header>
